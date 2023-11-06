53a64a4e44f9e08965647fb33793837a045744d0
alessandro <alessandrodamata6@gmail.com>
2023-11-06 18:05:24 -0300

    criado o component de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Header from "./components/Header";
-import Noticias from "./pages/noticias";
-import Login from "./pages/login";
-import Cursos from "./pages/cursos";
-import Horarios from "./pages/horarios";
-import Contatos from "./pages/contatos";
-import Servidores from "./pages/servidores";
+import { Rotas } from "./routes";
 const App = () => {
-	const logado =false;
 	return (
-		<BrowserRouter>
-			{logado && <Header />}
-			<Routes>
-				<Route  path="/" element={logado ? <Noticias /> : <Navigate to={"/login"} /> } />
-				<Route  path="/cursos" element={logado ? <Cursos /> : <Navigate to={"/login"} /> } />
-				<Route  path="/horarios" element={logado ? <Horarios /> : <Navigate to={"/login"} /> } />
-				<Route  path="/contatos" element={logado ? <Contatos /> : <Navigate to={"/login"} /> } />
-				<Route  path="/servidores" element={logado ? <Servidores /> : <Navigate to={"/login"} /> } />
-				<Route  path="/login" element={!logado ? <Login /> : <Navigate to={"/"} /> } />
-				
-			</Routes>
-		</BrowserRouter>
+		<Rotas />
 	);
 };
 
---
+++ src/routes/index.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Header from "../components/Header";
+import Noticias from "../pages/noticias";
+import Cursos from "../pages/cursos";
+import Contatos from "../pages/contatos";
+import Servidores from "../pages/servidores";
+import Login from "../pages/login";
+import Horarios from "../pages/horarios";
+export const Rotas = () => {
+	const logado = true;
+	return (
+		<BrowserRouter>
+			{logado && <Header />}
+			<Routes>
+				<Route  path="/" element={logado ? <Noticias /> : <Navigate to={"/login"} /> } />
+				<Route  path="/cursos" element={logado ? <Cursos /> : <Navigate to={"/login"} /> } />
+				<Route  path="/horarios" element={logado ? <Horarios /> : <Navigate to={"/login"} /> } />
+				<Route  path="/contatos" element={logado ? <Contatos /> : <Navigate to={"/login"} /> } />
+				<Route  path="/servidores" element={logado ? <Servidores /> : <Navigate to={"/login"} /> } />
+				<Route  path="/login" element={!logado ? <Login /> : <Navigate to={"/"} /> } />
+				
+			</Routes>
+		</BrowserRouter>
+	);
+};
