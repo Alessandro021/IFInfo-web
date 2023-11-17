4cc032a7d6df7954c61dc2aaa60d6c1063376409
alessandro <alessandrodamata6@gmail.com>
2023-11-16 22:01:13 -0300

    fix: removido a função de deslogar usario caso ele nao seja permitido, no arquivo api

--- src/services/api.js
+++ src/services/api.js
@@ -2,27 +2,28 @@ import axios from "axios";
 import { useStorage } from "../useHooks/useStorage";
 import { useUsuario } from "../store/useUsuario";
 
-const {pegar} = useStorage();
-const token = JSON.parse(pegar());
 
 const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
-	headers: {
-		Authorization: `Bearer ${token?.accessToken}`
-	}
 });
 
-api.interceptors.response.use(
-	response => response,
-	error => {
-		const deslogarUsuario = useUsuario(state => state.deslogarUsuario);
-		console.log("passei");
-		if (error.response.status === 401) {
-			deslogarUsuario();
-		}
-		return Promise.reject(error);
-	}
-);
+api.interceptors.request.use((config) => {
+	const {pegar} = useStorage();
+	const token = JSON.parse(pegar());
+	config.headers.Authorization = `Bearer ${token?.accessToken}`;
+	return config;
+});
+
+// api.interceptors.response.use(
+// 	response => response,
+// 	error => {
+// 		const deslogarUsuario = useUsuario(state => state.deslogarUsuario);
+// 		if (error.response.status === 401) {
+// 			deslogarUsuario();
+// 		}
+// 		return Promise.reject(error);
+// 	}
+// );
   
 
 export default api;
