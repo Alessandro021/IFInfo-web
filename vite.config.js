bd84574371315596f12d381dfeb8344dfe79547b
alessandro <alessandrodamata6@gmail.com>
2023-11-25 18:45:48 -0300

    feat: adicionado ao vite.config  a varfiavel de ambiente VITE_PORT

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+const env = loadEnv(
+	"all",
+	process.cwd()
+);
 const __dirname = path.dirname(__filename);
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 3001
+		port: env.VITE_PORT || 3001
 	},
 	resolve: {
 		alias: {
